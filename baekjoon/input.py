d43a599a3a4b2754c7e0ced957c90421fe2622ec
HyunMinSam <hyunmin4287@naver.com>
2021-07-03 23:08:08 +0900

    input and output

--- baekjoon/input.py
+++
@@ -1,9 +0,0 @@
-# input() 함수는 '한 줄'의 '문자열'을 입력 받음
-# map() 함수는 리스트의 모든 원소에 각각 특정한 함수를 적용할 떄 사용
-data = list(map(int, input().split()))
-data2 = (map(int, input().split()))
-
-print(data)
-print(data2)
-
-a, b, c = map(int, input().split())
---
+++ baekjoon/inputOutput.py
@@ -0,0 +1,28 @@
+# input() 함수는 '한 줄'의 '문자열'을 입력 받음
+# map() 함수는 리스트의 모든 원소에 각각 특정한 함수를 적용할 떄 사용
+
+# data = list(map(int, input().split()))
+# data2 = (map(int, input().split()))
+#
+# print(data)
+# print(data2)
+#
+# a, b, c = map(int, input().split())
+
+# 빠른 입력 sys.stdin.readline() + rstrip()은 줄바꿈 제거
+# print("hi")
+# import sys
+# data = list(map(int, sys.stdin.readline().rstrip().split()))
+# print(data)
+
+
+# Output
+print(7, end=" ")
+print(7, end=" ")
+answer = 7
+print("정답은 " + str(answer) + " 입니다", end="\n")
+
+# f-string
+answer = 7
+print(f"정답은 {answer} 입니다")
+
