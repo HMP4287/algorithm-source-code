3d9693fb2d5738ed6e0da5062c3249194578a302
HyunMinSam <hyunmin4287@naver.com>
2021-07-03 23:17:28 +0900

    if and things

---
+++ baekjoon/if.py
@@ -0,0 +1,14 @@
+# and or not, if elif else
+if 1 == 1 and 1 == 1:
+    print("hi")
+
+# in, not in -> 리스트 문자열 ...
+
+# pass 키워드
+
+# 조건부 표현식
+score = 59
+result = "Success" if score > 10 else "Fail"
+
+# 파이썬 조건문 내에서 부등식
+# if x > 0 and x < 20: -> if 0 < x < 2:
