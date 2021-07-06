5dfe1c04aa569f4ab9a26ad13aeffd67fc514a0b
HyunMinSam <hyunmin4287@naver.com>
2021-07-06 18:35:19 +0900

    CH11_6_Greedy python 풀이 테스트 케이스 모두 통과. 효율성 모두 불통과.

--- thisiscodingtest/CH11_6_Greedy.py
+++ thisiscodingtest/CH11_6_Greedy.py
@@ -1,18 +1,39 @@
+# def solution(food_times, k):
+#     temp = 0
+#     _max = len(food_times)
+#     while True:
+#         for i in range(_max):
+#             if food_times[i]:   # 존재 한다면
+#                 if temp > 0:
+#                     temp = 0
+#                 if k < 1: #    네트워크 고치고 먹을 음식
+#                     return i+1
+#                 food_times[i] -= 1
+#                 k -= 1
+#             else:
+#                 if temp >= _max:
+#                     return -1
+#                 else:
+#                     temp += 1
+#                     continue
+#
+
+
+# 개선 코드
+import heapq
+
 def solution(food_times, k):
-    temp = 0
-    _max = len(food_times)
-    while True:
-        for i in range(_max):
-            if food_times[i]:   # 존재 한다면
-                if temp > 0:
-                    temp = 0
-                if k < 1: #    네트워크 고치고 먹을 음식
-                    return i+1
-                food_times[i] -= 1
-                k -= 1
-            else:
-                if temp >= _max:
-                    return -1
-                else:
-                    temp += 1
-                    continue
+    if sum(food_times) <= k:
+        return -1
+
+
+    q = []
+    for i in range(len(food_times)):
+        heapq.heappush(q, (food_times[i], i+1))     # 튜플 형태로 우선순위 큐에 삽입
+
+    sum_value = 0
+    previous = 0
+    length = len(food_times)
+
+
+
