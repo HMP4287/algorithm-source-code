f2a893302ef903109a9e59c70d0985e0800c8402
HyunMinSam <hyunmin4287@naver.com>
2021-07-07 22:07:20 +0900

    2017 중앙대학교 프로그래밍 경진대회(CPC)

--- baekjoon/14730.py
+++ baekjoon/14730.py
@@ -1,16 +1,12 @@
 n = int(input())
 
+arr = []
 total = 0
 for _ in range(n):
     c, k = map(int, input().split())
+    if k > 0:
+        total += (c*k)
 
-    if k > 0 and c > 0:
-        temp = 0
-        c = (c * k)
-        k -= 1
-        if k == 0:
-            temp = c
-        else:
-            temp = (c * (2 ** k))
-        total += temp
-print(total % 10e9 + 7)
+print(total)
+
+
