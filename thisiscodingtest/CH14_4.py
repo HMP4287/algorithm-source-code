b7d00a3f4eead60e4967c0c0353c03aeb8c1b2c8
HyunMinSam <hyunmin4287@naver.com>
2021-07-27 21:38:04 +0900

    CH14_4

--- thisiscodingtest/CH14_4.py
+++ thisiscodingtest/CH14_4.py
@@ -1,10 +1,10 @@
 import heapq
+
 N = int(input())
 cards = []
 for _ in range(N):
     cards.append(int(input()))
 
-# 제귀 ?
 if N == 1:  # ex) N==1 cards = [1]일때 비교 횟수는 0임.
     print(0)
 elif N == 2:
@@ -12,29 +12,9 @@ elif N == 2:
 else:
     result = 0
     heapq.heapify(cards)
-    for _ in range(N-1):  # 연산 횟수는 총 N-1
+    for _ in range(N - 1):  # 연산 횟수는 총 N-1
         a, b = heapq.heappop(cards), heapq.heappop(cards)
         tot = a + b
         result += tot
         heapq.heappush(cards, tot)
     print(result)
-
-
-
-
-
-    # V1.0
-    # cards.sort()
-    # total = 0
-    # prev = 0
-    # for i in range(N-1):
-    #     added = cards[i] + cards[i + 1]
-    #     total += added
-    #     cards[i+1] = added
-    #     prev = added
-    # print(total)
-
-
-
-
-
